55ec3f70c9cd78e41c3c9999a08da6888e50cb89
Mr-Bogdan <martinivbogdan@gmail.com>
2022-08-11 19:20:47 +0300

    add ansible to repo

--- setup/CreateDatastore.sql
+++ setup/CreateDatastore.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE onlinebookstore;
+CREATE DATABASE IF NOT EXISTS onlinebookstore;
 
-\c onlinebookstore
+USE onlinebookstore;
 
-CREATE TABLE books 
+CREATE TABLE IF NOT EXISTS books 
   ( 
      barcode   VARCHAR(100) PRIMARY KEY, 
      name      TEXT NOT NULL, 
@@ -11,7 +11,7 @@ CREATE TABLE books
      quantity  REAL
   ); 
   
-  CREATE TABLE users
+CREATE TABLE IF NOT EXISTS users
   ( 
      username  VARCHAR(100) PRIMARY KEY, 
      password  VARCHAR(100) NOT NULL, 
@@ -22,3 +22,15 @@ CREATE TABLE books
      mailid    VARCHAR(100) NOT NULL,
      usertype  INT
   ); 
+
+
+insert into books values('10101','Programming in C','James k Wick',500,5);
+insert into books values('10102','Learn Java','Scott Mayers',150,13);
+insert into books values('10103','Database Knowledge','Charles Pettzoid',124,360);
+insert into books values('10104','Let us c++','Steve Macclen',90,111);
+insert into books values('10105','Success Key','Shashi Raj',5000,15);
+insert into users values('User','Password','First','User','My Home','42502216225','User@gmail.com',2);
+insert into users values('Admin','Admin','Mr.','Admin','Haldia WB','9584552224521','admin@gmail.com',1);
+insert into users values('shashi','shashi','Shashi','Raj','Bihar','1236547089','shashi@gmail.com',2);
+
+commit;
